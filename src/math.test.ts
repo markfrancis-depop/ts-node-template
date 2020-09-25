554ba2b8e0c330cac009f8c398a770178646679d
MarkSFrancis <mark.francis@cinch.co.uk>
2020-09-25 17:40:16 +0100

    Added describe test block

--- src/math.test.ts
+++ src/math.test.ts
@@ -1,5 +1,11 @@
 import { sum } from "./math";
 
-test("adds 1 + 2 to equal 3", () => {
-  expect(sum(1, 2)).toBe(3);
+describe("sum", () => {
+  test("adds 1 + 2 to equal 3", () => {
+    expect(sum(1, 2)).toBe(3);
+  });
+
+  test("adds 1 + -2 to equal -1", () => {
+    expect(sum(1, -2)).toBe(-1);
+  });
 });

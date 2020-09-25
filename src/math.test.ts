6ac35706c1105089f221da3ab64028c7ceb02ed6
MarkSFrancis <mark.francis@cinch.co.uk>
2020-09-25 17:57:40 +0100

    Added test for more params

--- src/math.test.ts
+++ src/math.test.ts
@@ -8,4 +8,8 @@ describe("sum", () => {
   test("adds 1 + -2 to equal -1", () => {
     expect(sum(1, -2)).toBe(-1);
   });
+
+  test("adds 1 + 2 + 3 to equal 6", () => {
+    expect(sum(1, 2, 3)).toBe(6);
+  });
 });
